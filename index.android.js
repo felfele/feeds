2c5502a6153545746071f0274567efacaa1a8dab
Attila Gazso <agazso@gmail.com>
2020-01-23 12:53:32 +0700

    Renamed in index.*.js as well

--- index.android.js
+++ index.android.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
-import FelfeleApp from './src/App';
+import Feeds from './src/App';
 
-AppRegistry.registerComponent('Feeds', () => FelfeleApp);
+AppRegistry.registerComponent('Feeds', () => Feeds);
