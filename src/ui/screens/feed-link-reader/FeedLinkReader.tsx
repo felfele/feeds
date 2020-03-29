35a32a3167be08a7af797f0298c39e2da8e1e42a
Attila Gazso <agazso@gmail.com>
2020-03-29 12:41:07 +0200

    Don't show QR code reader when the input is active on Add feed screen

--- src/ui/screens/feed-link-reader/FeedLinkReader.tsx
+++ src/ui/screens/feed-link-reader/FeedLinkReader.tsx
@@ -25,6 +25,7 @@ const QRCameraWidth = Dimensions.get('window').width;
 const QRCameraHeight = QRCameraWidth;
 
 interface State {
+    showCode: boolean;
 }
 
 export interface DispatchProps { }
@@ -35,9 +36,8 @@ export interface StateProps {
 
 type Props = DispatchProps & StateProps;
 
-const QRCodeButton = (props: {navigation: TypedNavigation}) => {
-    const [showCode, setShowCode] = React.useState(false);
-    return showCode
+const QRCodeButton = (props: {navigation: TypedNavigation, showCode: boolean, setShowCode: (showCode: boolean) => void}) => {
+    return props.showCode
         ?
             <QRCodeScanner
                 onRead={(event) => props.navigation.replace('RSSFeedLoader', { feedUrl: event.data })}
@@ -51,7 +51,7 @@ const QRCodeButton = (props: {navigation: TypedNavigation}) => {
                 label='SCAN QR CODE'
                 icon={<Icon name='qrcode' size={24} color={Colors.BRAND_PURPLE}/>}
                 onPress={() => {
-                    setShowCode(true);
+                    props.setShowCode(true);
                     Keyboard.dismiss();
                 }}
             />
@@ -59,6 +59,10 @@ const QRCodeButton = (props: {navigation: TypedNavigation}) => {
 };
 
 export class FeedLinkReader extends React.Component<Props, State> {
+    public state = {
+        showCode: false,
+    };
+
     public render() {
         const icon = (name: string, size: number = 20) =>
             <Icon name={name} size={size} color={ComponentColors.NAVIGATION_BUTTON_COLOR} />;
@@ -74,7 +78,7 @@ export class FeedLinkReader extends React.Component<Props, State> {
                     navigation={this.props.navigation}
                 />
                 <View style={styles.container}>
-                    <RegularText style={styles.hintLabel}>You can enter a link of a website or a blog here you want to follow. You can also import from Feeds or OPML links.</RegularText>
+                    <RegularText style={styles.hintLabel}>Enter a link of a website or a blog you want to follow. You can also import Feeds or OPML links.</RegularText>
                     <View style={{
                         flexDirection: 'row',
                     }}>
@@ -88,11 +92,16 @@ export class FeedLinkReader extends React.Component<Props, State> {
                             returnKeyType='done'
                             onSubmitEditing={(text) => this.handleLink(text)}
                             onEndEditing={() => {}}
+                            onFocus={() => this.setState({showCode: false})}
                         />
                     </View>
                     <RegularText style={styles.hintLabel}>You can also scan QR codes from other people.</RegularText>
                     <View style={styles.qrCameraContainer}>
-                        <QRCodeButton navigation={this.props.navigation} />
+                        <QRCodeButton
+                            navigation={this.props.navigation}
+                            showCode={this.state.showCode}
+                            setShowCode={showCode => this.setState({showCode})}
+                        />
                     </View>
                 </View>
             </FragmentSafeAreaView>
@@ -116,10 +125,6 @@ export class FeedLinkReader extends React.Component<Props, State> {
         }
         const feedUrl = text;
         this.props.navigation.replace('RSSFeedLoader', { feedUrl });
-    }
-
-    private onScanSuccess = (data: any) => {
-        this.handleLink(data);
     }
 }
 
