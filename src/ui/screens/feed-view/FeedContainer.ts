79cc22564916b97f9a07f12ed9c56497b40578b1
Attila Gazso <agazso@gmail.com>
2020-06-12 11:45:03 +0200

    Simplified FeedContainer logic

--- src/ui/screens/feed-view/FeedContainer.ts
+++ src/ui/screens/feed-view/FeedContainer.ts
@@ -7,7 +7,7 @@ import { Feed } from '../../../models/Feed';
 import { getFeedPosts } from '../../../selectors/selectors';
 import { TypedNavigation } from '../../../helpers/navigation';
 
-const emptyFeed = (name: string = '', isKnownFeed: boolean = false): Feed => ({
+const emptyFeed = (name: string = ''): Feed => ({
     name,
     feedUrl: '',
     url: '',
@@ -18,16 +18,8 @@ export const mapStateToProps = (state: AppState, ownProps: { navigation: TypedNa
     const feedUrl = ownProps.navigation.getParam<'Feed', 'feedUrl'>('feedUrl');
     const feedName = ownProps.navigation.getParam<'Feed', 'name'>('name');
 
-    const otherFeeds = state.feeds;
-    const otherFeed = otherFeeds.find(feed => feed.feedUrl === feedUrl);
-    const selectedFeed = otherFeed != null
-        ? otherFeed
-        : emptyFeed(feedName)
-    ;
-    // Note: this is a moderately useful selector (recalculated if another feedUrl is opened (cache size == 1))
-    // see https://github.com/reduxjs/reselect/blob/master/README.md#accessing-react-props-in-selectors
-    const feedPosts = getFeedPosts(state, feedUrl);
-    const posts = feedPosts;
+    const selectedFeed = state.feeds.find(feed => feed.feedUrl === feedUrl) || emptyFeed(feedName);
+    const posts = getFeedPosts(state, feedUrl);
     return {
         onBack: () => ownProps.navigation.popToTop(),
         navigation: ownProps.navigation,
