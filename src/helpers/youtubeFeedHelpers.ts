23ca0cae2a0a00a90d95b9ff7576b25d88e9b8e5
Attila Gazso <agazso@gmail.com>
2020-09-24 15:11:19 +0200

    Added Youtube link support

---
+++ src/helpers/youtubeFeedHelpers.ts
@@ -0,0 +1,25 @@
+import * as urlUtils from './urlUtils';
+import { Feed } from '../models/Feed';
+import { parse } from 'url';
+import { fetchFeedFromUrl } from './RSSPostHelpers';
+
+export const isYoutubeLink = (url: string): boolean => {
+    const canonicalUrl = urlUtils.getCanonicalUrl(url);
+    const humanHostName = urlUtils.getHumanHostname(canonicalUrl);
+    return humanHostName === 'youtube.com';
+};
+
+export const fetchYoutubeFeed = async (url: string): Promise<Feed | undefined> => {
+    const parsedUrl = parse(url);
+    if (parsedUrl.path?.startsWith('/channel/')) {
+        const channelId = parsedUrl.path?.replace('/channel/', '');
+        const feedUrl = `https://www.youtube.com/feeds/videos.xml?channel_id=${channelId}`;
+        const feed = await fetchFeedFromUrl(feedUrl);
+        if (feed != null) {
+            return feed;
+        } else {
+            return undefined;
+        }
+    }
+    return undefined;
+};
