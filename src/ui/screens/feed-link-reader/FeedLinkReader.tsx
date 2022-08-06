09a6ef37844da0caf51a1ff2b968bfdb06662dfb
Attila Gazso <agazso@gmail.com>
2022-08-06 16:58:13 +0200

    improve add feed screen

--- src/ui/screens/feed-link-reader/FeedLinkReader.tsx
+++ src/ui/screens/feed-link-reader/FeedLinkReader.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { useState, useEffect } from 'react'
 import {
     StyleSheet,
     View,
@@ -19,7 +20,8 @@ import { FragmentSafeAreaView } from '../../misc/FragmentSafeAreaView'
 import { getHttpLinkFromText } from '../../../helpers/urlUtils'
 import { FEEDS_LINK_MESSAGE } from '../../../helpers/linkHelpers'
 import { RegularText } from '../../misc/text'
-import { WideButton } from '../../buttons/WideButton'
+import { TwoButton } from '../../buttons/TwoButton'
+import { TouchableView } from '../../misc/TouchableView'
 
 const QRCameraWidth = Dimensions.get('window').width
 const QRCameraHeight = QRCameraWidth
@@ -36,96 +38,125 @@ export interface StateProps {
 
 type Props = DispatchProps & StateProps
 
-const QRCodeButton = (props: {navigation: TypedNavigation, showCode: boolean, setShowCode: (showCode: boolean) => void}) => {
+const ButtonContainer = (props: {
+    navigation: TypedNavigation,
+    showCode: boolean,
+    setShowCode: (showCode: boolean) => void,
+    onAddLink: () => void,
+    onClose: () => void,
+}) => {
     return props.showCode
         ?
-            <QRCodeScanner
-                onRead={(event) => props.navigation.replace('RSSFeedLoader', { feedUrl: event.data })}
-                containerStyle={styles.qrCameraStyle as any as RegisteredStyle<ViewStyle>}
-                cameraStyle={styles.qrCameraStyle as any as RegisteredStyle<ViewStyle>}
-                fadeIn={false}
-                cameraProps={{ratio: '1:1'}}
-            />
+            <View style={{...styles.container, paddingTop: 10}}>
+                <TouchableView onPress={props.onClose}>
+                    <Icon name='close' size={24} color={Colors.GRAY}/>
+                </TouchableView>
+                <QRCodeScanner
+                            onRead={(event) => props.navigation.replace('RSSFeedLoader', { feedUrl: event.data })}
+                            containerStyle={styles.qrCameraStyle as any as RegisteredStyle<ViewStyle>}
+                            cameraStyle={styles.qrCameraStyle as any as RegisteredStyle<ViewStyle>}
+                            fadeIn={false}
+                            cameraProps={{ratio: '1:1'}}
+                />
+            </View>
         :
-            <WideButton
-                label='SCAN QR CODE'
-                icon={<Icon name='qrcode' size={24} color={Colors.BRAND_PURPLE}/>}
-                onPress={() => {
-                    props.setShowCode(true)
-                    Keyboard.dismiss()
+            <TwoButton
+                leftButton={{
+                    label: 'Scan QR code',
+                    icon: <Icon name='qrcode' size={24} color={Colors.BRAND_PURPLE}/>,
+                    onPress: () => {
+                        props.setShowCode(true)
+                        Keyboard.dismiss()
+                    },
+                }}
+                rightButton={{
+                    label: 'Add link',
+                    icon: <Icon name='link' size={24} color={Colors.BRAND_PURPLE}/>,
+                    onPress: props.onAddLink,
                 }}
             />
-
 }
 
-export class FeedLinkReader extends React.Component<Props, State> {
-    public state = {
-        showCode: false,
-    }
+export function FeedLinkReader(props: Props) {
+    const [showCode, setShowCode] = useState(false)
+    const [linkText, setLinkText] = useState('')
+    const [textRef, setTextRef] = useState<SimpleTextInput | null>(null)
 
-    public render() {
-        const icon = (name: string, size: number = 20) =>
-            <Icon name={name} size={size} color={ComponentColors.NAVIGATION_BUTTON_COLOR} />
+    const icon = (name: string, size: number = 20) =>
+        <Icon name={name} size={size} color={ComponentColors.NAVIGATION_BUTTON_COLOR} />
 
-        return (
-            <FragmentSafeAreaView>
-                <NavigationHeader
-                    title={'Add feed'}
-                    leftButton={{
-                        label: icon('close', 24),
-                        onPress: () => this.props.navigation.goBack(null),
-                    }}
-                    navigation={this.props.navigation}
-                />
-                <View style={styles.container}>
-                    <RegularText style={styles.hintLabel}>Enter a link of a website or a blog you want to follow. You can also import Feeds or OPML links.</RegularText>
-                    <View style={{
-                        flexDirection: 'row',
-                    }}>
-                        <SimpleTextInput
-                            style={styles.linkInput}
-                            placeholder='Paste link here'
-                            placeholderTextColor={Colors.MEDIUM_GRAY}
-                            autoCapitalize='none'
-                            autoFocus={true}
-                            autoCorrect={false}
-                            returnKeyType='done'
-                            onSubmitEditing={(text) => this.handleLink(text)}
-                            onEndEditing={() => {}}
-                            onFocus={() => this.setState({showCode: false})}
-                        />
-                    </View>
-                    <RegularText style={styles.hintLabel}>You can also scan QR codes from other people.</RegularText>
-                    <View style={styles.qrCameraContainer}>
-                        <QRCodeButton
-                            navigation={this.props.navigation}
-                            showCode={this.state.showCode}
-                            setShowCode={showCode => this.setState({showCode})}
-                        />
-                    </View>
-                </View>
-            </FragmentSafeAreaView>
-        )
-    }
-
-    public async componentDidMount() {
-        const clipboardText = await Clipboard.getString()
-        if (clipboardText.startsWith(FEEDS_LINK_MESSAGE)) {
-            const link = getHttpLinkFromText(clipboardText)
-            if (link != null) {
-                this.props.navigation.replace('RSSFeedLoader', { feedUrl: link })
+    useEffect(() => {
+        Clipboard.getString().then(clipboardText => {
+            if (clipboardText.startsWith(FEEDS_LINK_MESSAGE)) {
+                const link = getHttpLinkFromText(clipboardText)
+                if (link != null) {
+                    props.navigation.replace('RSSFeedLoader', { feedUrl: link })
+                }
             }
-        }
-    }
+        })
+    }, [])
 
-    private handleLink(text: string) {
+    const handleLink = (text: string) => {
         if (text === '') {
-            this.props.navigation.goBack(null)
+            props.navigation.goBack(null)
             return
         }
         const feedUrl = text
-        this.props.navigation.replace('RSSFeedLoader', { feedUrl })
+        props.navigation.replace('RSSFeedLoader', { feedUrl })
     }
+
+    const focusLinkText = () => {
+        if (textRef) {
+            (textRef as SimpleTextInput).focus()
+        }
+    }
+
+    return (
+        <FragmentSafeAreaView>
+            <NavigationHeader
+                title={'Add feed'}
+                leftButton={{
+                    label: icon('close', 24),
+                    onPress: () => props.navigation.goBack(null),
+                }}
+                navigation={props.navigation}
+            />
+            <View style={styles.container}>
+                <RegularText style={styles.hintLabel}>Enter a link of a website or a blog you want to follow. </RegularText>
+                <RegularText style={styles.hintLabel}>You can also import Feeds or OPML links or scan QR codes from other people.</RegularText>
+                <View style={{
+                    flexDirection: 'row',
+                }}>
+                    <SimpleTextInput
+                        style={styles.linkInput}
+                        placeholder='Paste link here'
+                        placeholderTextColor={Colors.MEDIUM_GRAY}
+                        autoCapitalize='none'
+                        autoFocus={true}
+                        autoCorrect={false}
+                        returnKeyType='done'
+                        onSubmitEditing={(text) => handleLink(text)}
+                        onEndEditing={() => {}}
+                        onFocus={() => setShowCode(false)}
+                        onChangeText={text => setLinkText(text)}
+                        ref={value => setTextRef(value)}
+                    />
+                </View>
+                <View style={styles.qrCameraContainer}>
+                    <ButtonContainer
+                        navigation={props.navigation}
+                        showCode={showCode}
+                        setShowCode={sc => setShowCode(sc)}
+                        onAddLink={() => handleLink(linkText)}
+                        onClose={() => {
+                            setShowCode(false)
+                            focusLinkText()
+                        }}
+                    />
+                </View>
+            </View>
+        </FragmentSafeAreaView>
+    )
 }
 
 const styles = StyleSheet.create({
